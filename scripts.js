b58f1956c4751da8e9b27be442e3fb1e1f373be5
Marlisa Odokara <marlisa.od@gmail.com>
2025-01-27 16:57:26 -0600

    completed js for ip address tracker

--- scripts.js
+++ scripts.js
@@ -5,12 +5,19 @@ const iPAddress = document.getElementById('ip-address');
 const location = document.getElementById('location');
 const timezone = document.getElementById('timezone');
 const isp = document.getElementById('isp');
+let map = document.getElementById('map');
+
+map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 let latitude;
 let longitude;
 
-
-searchBtn.addEventListener('click', () => {
+function renderResults () {
     const API_KEY="at_JzzyA6OORXKXkjSIvf0BAKq0qI5EG";
     let url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${searchInput.value}`
 
@@ -23,10 +30,19 @@ searchBtn.addEventListener('click', () => {
         timezone.textContent = `UTC ${data.location.timezone}`;
         isp.textContent = data.isp;
 
+        latitude = data.location.lat;
+        longitude = data.location.lng;
+
+        updateMap(latitude, longitude);
     })
     .catch((err) => {
         console.log(err);
     })
+}
 
+const updateMap = (latitude, longitude) => {
+    map.setView([latitude, longitude], 13);
+    L.marker([latitude, longitude]).addTo(map);
+}
 
-})
+searchBtn.addEventListener('click', renderResults);
